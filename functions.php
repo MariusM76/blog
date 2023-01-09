e0c2a8dd759a606b115446042956fa3c9360dd0f
Marius Marian <marius25@yahoo.com>
2023-01-09 14:47:17 +0200

    modfied Post class and functions

--- functions.php
+++ functions.php
@@ -39,3 +39,29 @@ function searchArray($array,$needle){
     }
     return $result;
 }
+
+function format_interval(DateInterval $interval) {
+    $result = "";
+    if ($interval->y) { $result .= $interval->format("%y years "); }
+    if ($interval->m) { $result .= $interval->format("%m months "); }
+    if ($interval->d) { $result .= $interval->format("%d days "); }
+    if ($interval->h) { $result .= $interval->format("%h hours "); }
+    if ($interval->i) { $result .= $interval->format("%i minutes "); }
+//    if ($interval->s) { $result .= $interval->format("%s seconds "); }
+
+    return $result;
+}
+
+function lastUpdateShow($id){
+    $post = new Post($id);
+//    if ($post->updatedAt!=null){
+//        $updateDate = $post->updatedAt;
+//    } else {
+//        $updateDate = $post->createdAt;
+//    }
+    $date1 = new DateTime("now");
+    $date2 = new DateTime($post->createdAt);
+    $interval = $date1->diff($date2);
+    return format_interval($interval);
+
+}
